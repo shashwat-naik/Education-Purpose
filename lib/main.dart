7d7e36f645ede31b546551fec07ea447d26d3f2a
shashwat-naik <naikshashwat3@gmail.com>
2025-04-03 23:54:28 +0530

    News Section

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,12 @@ class HomeScreen extends StatelessWidget {
     '#SelfCare',
     '#Happiness'
   ];
+  final List<String> articleImg = [
+    'assets/article_img.png',
+    'assets/article_img1.png',
+    'assets/article_img2.png',
+    'assets/article_img3.png',
+  ];
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -159,7 +165,123 @@ class HomeScreen extends StatelessWidget {
                 );
               },
             ),
-          )
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 304,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: articleImg.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.all(12),
+                  margin: EdgeInsets.only(right: 20),
+                  height: 304,
+                  width: 255,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                    color: kWhite,
+                    boxShadow: [
+                      BoxShadow(
+                        color: kDarkBlue.withOpacity(0.051),
+                        offset: Offset(0.0, 3.0),
+                        blurRadius: 24.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 184,
+                        // height: 164,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          color: kWhite,
+                          image: DecorationImage(
+                              image: AssetImage(articleImg[index]),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18,
+                      ),
+                      Flexible(
+                        child: Text(
+                          'Feel the thrill on the only surf simulator in Lakshadweep 2025.',
+                          style: kPoppinsBold.copyWith(
+                            fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 19,
+                                backgroundColor: kLightBlue,
+                                backgroundImage: AssetImage(
+                                  'assets/avatar1.png',
+                                ),
+                              ),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Sang Dong-Min',
+                                    style: kPoppinsSemibold.copyWith(
+                                      fontSize:
+                                          SizeConfig.blockSizeHorizontal! * 3,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Dec 15,2025',
+                                    style: kPoppinsRegular.copyWith(
+                                        color: kGrey,
+                                        fontSize:
+                                            SizeConfig.blockSizeHorizontal! *
+                                                2.5),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          Container(
+                            height: 38,
+                            width: 38,
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.circular(kBorderRadius),
+                              color: kLightWhite,
+                            ),
+                            child: Image.asset('assets/share.png'),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
         ],
       ),
     );
